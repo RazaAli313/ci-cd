a228c616270f37e0714d26a35213bc18f90fb25b
RazaAli313 <razaalipk313@gmail.com>
2025-08-13 04:03:51 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", (req, res, next) => {
       </head>
     </html>
     <body>
-      <h1>Hello I am Syed Muhammad Raza Ali Zaidi...</h1>
+      <h1>Hello I am web server powered by Syed Muhammad Raza Ali Zaidi ...</h1>
     </body>
     `);
   next();
